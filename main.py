637dd43a579075464761e2c6879517fdd8c2c6c4
dravi <d.ravi@ucl.ac.uk>
2019-09-19 10:45:26 +0100

    New function to assembly Mri after generation"

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import argparse
 from progression_net import progression_net
 import pickle
 import pprint
+import numpy as np
+from matplotlib.pyplot import imread
+import nibabel as nib
+from scipy.misc import imread
 
 environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
@@ -31,7 +35,7 @@ parser.add_argument('--slice', type=int, default=100, help='slice')
 FLAGS = parser.parse_args()
 
 
-def train_lin_regress():
+def train_regressors():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     maxNumberOfRegion = 1000
@@ -55,13 +59,15 @@ def main(_):
     conditioned_enabled = True
     if FLAGS.phase == 0:
         exit()
-        #train_lin_regress()
+        # train_regressors()
     if FLAGS.phase == 1:
         training(FLAGS.slice)
     if FLAGS.phase == 2:
-        transfer_learning(FLAGS.slice, conditioned_enabled,'sample_TF', 100)
+        transfer_learning(FLAGS.slice, conditioned_enabled, 'sample_TF', 1000)
     if FLAGS.phase == 3:
         testing(FLAGS.slice, conditioned_enabled, 'test_TF_CD')
+    if FLAGS.phase == 4:
+        assemblyMri('71.0712_1_2_1_ADNI_126_S_5243_MR_MT1__N3m_Br_20130724140336799_S195168_I382272.nii.png')
 
 
 def testing(curr_slice, conditioned_enabled, output_dir):
@@ -80,7 +86,7 @@ def testing(curr_slice, conditioned_enabled, output_dir):
         )
         print('\n\tTesting Mode')
         model.testing(
-            testing_samples_dir='./data/' + FLAGS.datasetTL +'/',
+            testing_samples_dir='./data/' + FLAGS.datasetTL + '/',
             current_slice=curr_slice,
             conditioned_enabled=conditioned_enabled
         )
@@ -103,13 +109,34 @@ def transfer_learning(curr_slice, conditioned_enabled, output_dir, num_epochs):
         )
         print('\n\tTransfer learning mode')
         model.train(
-            num_epochs=num_epochs,  # number of epochs
+            num_epochs=num_epochs,
             use_trained_model=FLAGS.use_trained_model,
             use_init_model=FLAGS.use_init_model,
             n_epoch_to_save=num_epochs,
             conditioned_enabled=conditioned_enabled,
         )
     tf.reset_default_graph()
+
+
+def assemblyMri(fileName):
+    currSliceSlice = 103
+    numb_Slice = 30
+    a = np.ones((5, 1280, 1280), dtype=np.float)
+    finalMri = np.ones((numb_Slice, 128, 128), dtype=np.int16)
+    for i in range(0, numb_Slice):
+        a[0, :, :] = imread('./save/' + str(currSliceSlice - 2) + '/test/test_2_' + fileName)
+        a[1, :, :] = imread('./save/' + str(currSliceSlice - 1) + '/test/test_1_' + fileName)
+        a[2, :, :] = imread('./save/' + str(currSliceSlice) + '/test/test_0_' + fileName)
+        a[3, :, :] = imread('./save/' + str(currSliceSlice + 1) + '/test/test_-1_' + fileName)
+        a[4, :, :] = imread('./save/' + str(currSliceSlice + 2) + '/test/test_-2_' + fileName)
+        MRIAll = a[0, :, :] * 0.05 + a[1, :, :] * 0.15 + a[2, :, :] * 0.6 + a[3, :, :] * 0.15 + a[4, :, :] * 0.05
+
+        index_progression = 9
+        finalMri[i, :, :] = np.int16(MRIAll[:128, 128 * index_progression:128 * (index_progression + 1)])
+
+        currSliceSlice = currSliceSlice + 1
+        img = nib.Nifti1Image(finalMri, np.eye(4))
+        nib.save(img, 'out.nii.gz')
 
 
 def training(curr_slice):
@@ -143,6 +170,7 @@ def training(curr_slice):
             use_init_model=FLAGS.use_init_model
         )
     tf.reset_default_graph()
+
 
 if __name__ == '__main__':
     # with tf.device('/cpu:0'):
