89efa9f46d7dc8bd2a0d91aeda2b7716a7111681
dravi <d.ravi@ucl.ac.uk>
2019-09-23 22:10:58 +0100

    removing redondant code, including classifier parameter

--- progression_net.py
+++ progression_net.py
@@ -3,12 +3,12 @@ import numpy as np
 import pickle
 from sklearn.pipeline import make_pipeline
 import sklearn.preprocessing as preprocessing
-#from sklearn.svm import SVR
+from sklearn.svm import SVR
 from sklearn.linear_model import LogisticRegression
 
 
 class progression_net(object):
-    def __init__(self, current_slice, max_regional_expansion, map_disease):
+    def __init__(self, current_slice, max_regional_expansion, map_disease, classifier):
         self.num_output = 1
         self.num_inputs = 4
         self.current_slice = current_slice
@@ -17,46 +17,47 @@ class progression_net(object):
         self.load_matlab_data(current_slice)
         self.max_regional_expansion = max_regional_expansion
         self.map_disease = map_disease
+        self.classifier = classifier
 
-    def test(self, i):
+    def load_regressor(self):
         filehandler = open('Regressor/' + str(self.current_slice), 'rb')
-        [allRegressor, allScaler] = pickle.load(filehandler)
+        [all_regressor, all_scaler] = pickle.load(filehandler)
         filehandler.close()
+        return all_regressor, all_scaler
 
-        indexForCurrentRegion = (self.in_data[:, 3] == i + 1)
+    def load_region_data(self, current_region):
+        indexForCurrentRegion = (self.in_data[:, 3] == current_region + 1)
         X = np.float64(self.in_data[indexForCurrentRegion, :3])
         for j in range(np.size(X, 0)):
             X[j, 2] = self.map_disease[np.int(X[j, 2]) - 1] + 1
-        Y = self.out_data[indexForCurrentRegion, :3]
-        result = allRegressor[i].predict_proba(X)
+        Y = self.out_data[indexForCurrentRegion]
+        return X, Y
+
+    def test(self, current_region):
+        [all_regressor, all_scaler] = self.load_regressor()
+        X, Y = self.load_region_data(current_region)
+        result = all_regressor[current_region].predict_proba(X)
         prediction = np.reshape(result[:, 0], [-1, 1])
-        print(np.concatenate((X[:50, :], Y[:50], allScaler[i].inverse_transform(prediction[:50])), axis=1))
+        print(np.concatenate((X[:50, :], Y[:50], all_scaler[current_region].inverse_transform(prediction[:50])), axis=1))
         return
 
-    def train_and_save(self, i):
-        filehandler = open('Regressor/' + str(self.current_slice), 'rb')
-        [allRegressor, allScaler] = pickle.load(filehandler)
-        filehandler.close()
-
-        indexForCurrentRegion = (self.in_data[:, 3] == i + 1)
-        X = np.float64(self.in_data[indexForCurrentRegion, :3])
-        for j in range(np.size(X, 0)):
-            X[j, 2] = self.map_disease[np.int(X[j, 2]) - 1] + 1
-
-        y = self.out_data[indexForCurrentRegion]
+    def train_and_save(self, current_region):
+        [all_regressor, all_scaler] = self.load_regressor()
+        X, y = self.load_region_data(current_region)
         corectIndex = [y < self.max_regional_expansion]
 
         y = y[np.reshape(corectIndex, (-1))]
         X = X[np.reshape(corectIndex, (-1)), :]
-        # clf = SVR(C=100, coef0=0.0, degree=2, epsilon=0.005, gamma='auto',
-        #          kernel='linear', max_iter=-1, shrinking=True, tol=0.000001, verbose=True)
-
-        clf = LogisticRegression(max_iter=100000000, tol=0.000001,
-                                 C=1.0, class_weight='balanced', fit_intercept=True,
-                                 intercept_scaling=1, multi_class='ovr',
-                                 penalty='l2', random_state=None, solver='lbfgs',
-                                 verbose=1, warm_start=False
-                                 )
+        if self.classifier == 0:
+            clf = SVR(C=100, coef0=0.0, degree=1, epsilon=0.005, gamma='auto',
+                      kernel='linear', max_iter=-1, shrinking=True, tol=0.000001, verbose=True)
+        else:
+            clf = LogisticRegression(max_iter=100000000, tol=0.000001,
+                                     C=1.0, class_weight='balanced', fit_intercept=True,
+                                     intercept_scaling=1, multi_class='ovr',
+                                     penalty='l2', random_state=None, solver='lbfgs',
+                                     verbose=1, warm_start=False
+                                     )
 
         np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
@@ -71,10 +72,10 @@ class progression_net(object):
 
         pipeline.fit(X, y_fina.ravel())
 
-        allRegressor.append(pipeline)
-        allScaler.append(y_n)
+        all_regressor.append(pipeline)
+        all_scaler.append(y_n)
         filehandler = open('Regressor/' + str(self.current_slice), 'wb')
-        pickle.dump([allRegressor, allScaler], filehandler)
+        pickle.dump([all_regressor, all_scaler], filehandler)
         filehandler.close()
         return 1
 
